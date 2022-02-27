15999d34bfc4cc140dee9a9b901c2631907c2bab
yllipetrovci <yllipetrovci@gmail.com>
2022-02-27 23:48:10 +0100

    Add init Calculator Screen

---
+++ src/screens/CalculatorScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CalculatorScreen = () => {
+    return (
+        <View>
+            <Text>CalculatorScreen</Text>
+        </View>
+    )
+};
+
+export default CalculatorScreen; 

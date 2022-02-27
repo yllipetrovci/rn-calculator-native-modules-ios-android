a5f17ded3233ada7f0afadb35fc9034af2195ab1
yllipetrovci <yllipetrovci@gmail.com>
2022-02-27 23:48:46 +0100

    Add init WebView Screen

---
+++ src/screens/WebViewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const WebViewScreen = () => {
+    return (
+        <View>
+            <Text>WebViewScreen</Text>
+        </View>
+    )
+};
+
+export default WebViewScreen; 

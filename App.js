9717018c81a7965ed8f16a87e716aae775166ec2
yllipetrovci <yllipetrovci@gmail.com>
2022-02-27 23:57:25 +0100

    Add DrawerNavigator

--- App.js
+++ App.js
@@ -1,39 +1,13 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import DrawerNavigator from './src/navigators/DrawerNavigator';
 
-function Feed() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed Screen</Text>
-    </View>
-  );
-}
-
-function Article() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Article Screen</Text>
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
-
-function MyDrawer() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
-    </Drawer.Navigator>
-  );
-}
 
 export default function App() {
+
   return (
     <NavigationContainer>
-      <MyDrawer />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }

fa57f44d01e47b3b49edbdd5711ba08bf9682f31
yllipetrovci <yllipetrovci@gmail.com>
2022-02-27 23:52:27 +0100

    Add states for Calculator screen

--- src/screens/CalculatorScreen.js
+++ src/screens/CalculatorScreen.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 
 const CalculatorScreen = () => {
+    const [firstInputValue, setFirstInputValue] = useState(0);
+    const [secondInputValue, setSecondInputValue] = useState(0);
+    const [result, setResult] = useState(0);
+
     return (
         <View>
             <Text>CalculatorScreen</Text>

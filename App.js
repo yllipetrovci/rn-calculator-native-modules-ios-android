69a2e7eeaa956564dc10f0c94eb0d901903daca3
yllipetrovci <yllipetrovci@gmail.com>
2022-02-27 23:11:13 +0100

    Init App

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

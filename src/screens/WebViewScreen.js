d8d733020ff647f1127e5f16fdb92525a56e142e
yllipetrovci <yllipetrovci@gmail.com>
2022-02-28 00:03:01 +0100

    Add WebView strcuture screen

--- src/screens/WebViewScreen.js
+++ src/screens/WebViewScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { Platform, View, Text } from 'react-native';
 
 const WebViewScreen = () => {
+    const isIOS = Platform.OS === 'ios';
+
     return (
-        <View>
-            <Text>WebViewScreen</Text>
-        </View>
+        <>
+            {isIOS ?
+                <WebView source={{ uri: 'https://www.taboola.com' }} /> :
+                <View>
+                    <Text>Android</Text>
+                </View>
+            }
+        </>
     )
 };
 
-export default WebViewScreen; 
+export default WebViewScreen;

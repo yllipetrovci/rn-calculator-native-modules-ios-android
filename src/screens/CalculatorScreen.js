f9acabe4c91d496f052958a50d4ee3e6c9ebe609
yllipetrovci <yllipetrovci@gmail.com>
2022-02-27 23:53:55 +0100

    Add structure of Calculator Screen

--- src/screens/CalculatorScreen.js
+++ src/screens/CalculatorScreen.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Button,
+    View,
+    Text
+} from 'react-native';
+import NumberInput from '../component/NumberInput';
 
 const CalculatorScreen = () => {
     const [firstInputValue, setFirstInputValue] = useState(0);
@@ -7,10 +14,47 @@ const CalculatorScreen = () => {
     const [result, setResult] = useState(0);
 
     return (
-        <View>
-            <Text>CalculatorScreen</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.form}>
+                <NumberInput
+                    onChangeNumber={setFirstInputValue}
+                    label="First Number"
+                    value={firstInputValue}
+                    placeholder="Write numbers"
+                />
+                <NumberInput
+                    onChangeNumber={setSecondInputValue}
+                    label="Second Number"
+                    value={secondInputValue}
+                    placeholder="Write numbers"
+                />
+
+                <Button
+                    onPress={onPressGenerateResult}
+                    title="Generate Result"
+                    color="red"
+                    // disabled={true}
+                    accessibilityLabel="Learn more about this purple button"
+                />
+
+                <Text style={{ fontSize: 20, fontWeight: '600' }}>Result: {result}</Text>
+            </View>
+
+        </SafeAreaView>
     )
 };
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+        flexDirection: 'column'
+    },
+    form: {
+        borderWidth: 1,
+        borderRadius: 4,
+        margin: 10,
+        padding: 10
+    }
+});
 
 export default CalculatorScreen; 

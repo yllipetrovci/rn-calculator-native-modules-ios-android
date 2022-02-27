db1d80b8bdc45cf1fe92858ccca5711b514baad1
yllipetrovci <yllipetrovci@gmail.com>
2022-02-27 23:51:40 +0100

    Add final implementation for NumberInput component

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 
-const NumberInput = () => {
+const NumberInput = ({ onChangeNumber, label, value, placeholder }) => {
     return (
-        <View>
-            <Text>NumberInput</Text>
+        <View style={styles.container} >
+            <Text style={styles.lableTxt}>{label}</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeNumber}
+                placeholder={placeholder}
+                keyboardType='numeric'
+                value={value + ""}
+            />
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 10
+    },
+    input: {
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 10,
+    },
+    lableTxt: {
+        fontWeight: '500',
+    }
+});
 
 export default NumberInput;

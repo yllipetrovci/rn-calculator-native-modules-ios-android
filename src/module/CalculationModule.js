430535aaa30f483bbf9e49605b508b1b1b35e264
yllipetrovci <yllipetrovci@gmail.com>
2022-02-28 01:44:04 +0100

    Add Android module

--- src/module/CalculationModule.js
+++ src/module/CalculationModule.js
@@ -1,15 +1,16 @@
 import { NativeModules, Platform } from "react-native";
 
-const { IOSCalculatorModule } = NativeModules;
+const { IOSCalculatorModule, AndroidCalculatorModule } = NativeModules;
+
 class CalculationModule {
 
 
     getModule() {
-        return Platform.OS === 'ios' ? IOSCalculatorModule : null;
+        return Platform.OS === 'ios' ? IOSCalculatorModule : AndroidCalculatorModule;
     }
 
     calculate(firstNumber, secondNumber, callback) {
-        this.getModule() !== null && this.getModule().open(firstNumber, secondNumber, (res)=> callback(res))
+        this.getModule() !== null && this.getModule().open(firstNumber, secondNumber, (res) => callback(res))
     }
 
 
